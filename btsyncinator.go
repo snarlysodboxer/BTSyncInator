510f3a86f9885729097f0d410fde62c02537479f
david amick <github@davidamick.com>
2014-02-10 22:40:57 -0800

    Add up/download speeds, prepare to use publickey auth for portforward.

--- btsyncinator.go
+++ btsyncinator.go
@@ -3,13 +3,13 @@ package main
 import (
   //"fmt"
   btsync "github.com/vole/btsync-api"
-  //"code.google.com/p/go.crypto/ssh"
   "log"
   "net/http"
   "html/template"
   "flag"
   "code.google.com/p/goconf/conf"
   "os"
+  //"github.com/snarlysodboxer/btsyncinator/portforward"
 )
 
 var (
@@ -24,13 +24,6 @@ type Folder struct {
   Secrets     *btsync.GetSecretsResponse
 }
 
-type APIData struct {
-  Error       error
-  Folders     []Folder
-  OS          string
-  Preferences *btsync.GetPreferencesResponse
-}
-
 type DaemonAPI struct {
   Name        string
   FQDN        string
@@ -39,7 +32,47 @@ type DaemonAPI struct {
   APIData     APIData
 }
 
+type APIData struct {
+  Error       error
+  Folders     []Folder
+  OS          string
+  Preferences *btsync.GetPreferencesResponse
+  Speeds      *btsync.GetSpeedResponse
+}
+
 type DaemonAPIs []DaemonAPI
+
+func loadAPIData(localPort int) APIData {
+  api := btsync.New("", "", localPort, false)
+  data := APIData{}
+  data.Error = nil
+  folders, err := loadFolders(api)
+  if err != nil {
+    data.Error = err
+  }
+  data.Folders = folders
+  // Get the OS:
+  os, err := api.GetOS()
+  if err != nil {
+    data.Error = err
+  }
+  data.OS = os.Name
+  // Get Upload and Download speed
+  speeds, err := api.GetSpeed()
+  if err != nil {
+    data.Error = err
+  }
+  data.Speeds = speeds
+  // Get general Preferences:
+  preferences, err := api.GetPreferences()
+  if err != nil {
+    // TODO: Fix "json: cannot unmarshal number into Go value of type bool" bug
+    //data.Error = err
+    //fmt.Printf("Error with GetPreferences! %s", err)
+  }
+  data.Preferences = preferences
+  return data
+}
 
 func loadFolders(api *btsync.BTSyncAPI) ([]Folder, error) {
   var heir error = nil
@@ -63,35 +96,9 @@ func loadFolders(api *btsync.BTSyncAPI) ([]Folder, error) {
       heir = err
     }
     fldr.Secrets = secrets
-    fldrs = ExtendFoldersSlice(fldrs, *fldr)
+    fldrs = append(fldrs, *fldr)
   }
   return fldrs, heir
-}
-
-func loadAPIData(localPort int) APIData {
-  api := btsync.New("", "", localPort, false)
-  data := APIData{}
-  data.Error = nil
-  folders, err := loadFolders(api)
-  if err != nil {
-    data.Error = err
-  }
-  data.Folders = folders
-  // Get the OS:
-  os, err := api.GetOS()
-  if err != nil {
-    data.Error = err
-  }
-  data.OS = os.Name
-  // Get general Preferences:
-  preferences, err := api.GetPreferences()
-  if err != nil {
-    // TODO: Fix "json: cannot unmarshal number into Go value of type bool" bug
-    //data.Error = err
-    //fmt.Printf("Error with GetPreferences! %s", err)
-  }
-  data.Preferences = preferences
-  return data
 }
 
 func loadDaemonAPIs() DaemonAPIs {
@@ -105,7 +112,7 @@ func loadDaemonAPIs() DaemonAPIs {
     daemonAPI.DaemonPort, _ = config.GetInt(section, "daemon_port")
     daemonAPI.LocalPort = 9000 + index
     daemonAPI.APIData = loadAPIData(daemonAPI.LocalPort)
-    daemonAPIs = ExtendDaemonAPIsSlice(daemonAPIs, daemonAPI)
+    daemonAPIs = append(daemonAPIs, daemonAPI)
   }
   return daemonAPIs
 }
@@ -162,34 +169,6 @@ func configDeleteHandler(writer http.ResponseWriter, request *http.Request) {
   } else {
     log.Fatal(writer, "Error with RemoveSection!")
   }
-}
-
-func ExtendDaemonAPIsSlice(slice []DaemonAPI, element DaemonAPI) []DaemonAPI {
-    n := len(slice)
-    if n == cap(slice) {
-        // Slice is full; must grow.
-        // We double its size and add 1, so if the size is zero we still grow.
-        newSlice := make([]DaemonAPI, len(slice), 2*len(slice)+1)
-        copy(newSlice, slice)
-        slice = newSlice
-    }
-    slice = slice[0 : n+1]
-    slice[n] = element
-    return slice
-}
-
-func ExtendFoldersSlice(slice []Folder, element Folder) []Folder {
-    n := len(slice)
-    if n == cap(slice) {
-        // Slice is full; must grow.
-        // We double its size and add 1, so if the size is zero we still grow.
-        newSlice := make([]Folder, len(slice), 2*len(slice)+1)
-        copy(newSlice, slice)
-        slice = newSlice
-    }
-    slice = slice[0 : n+1]
-    slice[n] = element
-    return slice
 }
 
 func main() {
